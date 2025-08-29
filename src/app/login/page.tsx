3b86712222b0797e81c2c3704e53c0d11a4115e0
Eduarda Cristina <eduarda.blue03@gmail.com>
2025-08-29 12:58:46 +0000

    segue a correção desse novo erro a principio o anterior corrigiu hook.js

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,23 +3,6 @@ import { Suspense } from 'react';
 import { LoginForm } from '@/components/auth/login-form';
 import AuthLayout from '@/components/layouts/auth-layout';
 
-function LoginPageContent({
-  message,
-  error,
-}: {
-  message?: string;
-  error?: string;
-}) {
-  return (
-    <AuthLayout
-      title="Acesse sua Conta"
-      description="Insira suas credenciais para acessar sua conta."
-    >
-      <LoginForm message={message} error={error} />
-    </AuthLayout>
-  );
-}
-
 export default function LoginPage({
   searchParams,
 }: {
@@ -27,10 +10,12 @@ export default function LoginPage({
 }) {
   return (
     <Suspense fallback={<div>Carregando...</div>}>
-      <LoginPageContent
-        message={searchParams.message}
-        error={searchParams.error}
-      />
+      <AuthLayout
+        title="Acesse sua Conta"
+        description="Insira suas credenciais para acessar sua conta."
+      >
+        <LoginForm message={searchParams.message} error={searchParams.error} />
+      </AuthLayout>
     </Suspense>
   );
 }
